745e9e976519c7b530372164d85c26f7c0dcd20f
nasuda <nasuda908@gmail.com>
2023-08-15 10:40:26 +0900

    add class

--- main.js
+++ main.js
@@ -13,3 +13,25 @@ para.innerText = 'create'
 const button = createDom('button', 'firstBtn');
 document.body.appendChild(button);
 button.textContent = 'Click';
+
+class Element {
+  constructor(elm, text) {
+    this.elm = elm
+    this.text = text
+  }
+  
+  create() {
+    const newElm = document.createElement(this.elm);
+    newElm.textContent = this.text
+    //document.root.appendChild(newElm)
+    return newElm
+  }
+
+}
+
+const head = new Element('h2', 'header')
+console.log(head);
+const title = head.create();
+console.log(title);
+document.body.appendChild(title);
+//document.body.appendChild(title);
